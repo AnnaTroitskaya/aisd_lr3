b7d31554a2ca2238a8b0977947ea607a4ea2397a
AnnaTroitskaya <150855428+AnnaTroitskaya@users.noreply.github.com>
2023-11-14 23:52:22 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue> 
+#include "avl_tree.h"
+#include "binary_search_tree.h"
+#include "rb_tree.h"
+#include "compare.h"
+using namespace std;
+
+int main()
+{
+	menu();
+	return 0;
+
+}
